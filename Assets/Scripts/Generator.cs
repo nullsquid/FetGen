39929c85c4b465649d0175fc329c020348f4eb74
Azrael <j.shepard.ramsay@gmail.com>
2015-03-21 13:33:42 -0400

    first local commit

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Generator : MonoBehaviour {
+	public FetishDB fetishes;
+	public Text fetText;
+
+	string noun;
+	string verb;
+	string flavor;
+
+	int nIndex;
+	int vIndex;
+	int fIndex;
+	// Use this for initialization
+	void Start () {
+
+
+		//GenerateFetish(noun, verb, flavor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void GenerateFetish(){
+		//Debug.Log(v + " " + n + " " + f);
+		noun = fetishes.nouns[Random.Range(0, fetishes.nouns.Count)];
+		verb = fetishes.verbs[Random.Range(0, fetishes.verbs.Count)];
+		flavor = fetishes.flavor[Random.Range(0, fetishes.flavor.Count)];
+
+		fetText.text = verb + " " + noun + " " + flavor;
+	}
+}
